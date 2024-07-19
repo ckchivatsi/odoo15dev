f0b70fa97427f88de8e21e3f759e3c0b31d0311f
Kelvin Chivatsi <kelvinchivatsi@hotmail.com>
2024-07-19 05:47:43 +0300

    Initial Commit - created first module: om_hospital

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': [],
+    'data': [],
+    'demo':[],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
