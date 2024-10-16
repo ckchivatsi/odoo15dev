af95012cc2ddd978098925db4ef81b1b5ec3f04c
ckchivatsi <kelvinchivatsi@hotmail.com>
2024-10-16 15:49:47 +0300

    Apply domain for menu in Odoo

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -12,4 +12,4 @@ class HospitalPatient(models.Model):
             ('male', 'Male'),
             ('female', 'Female')
         ], string='Gender')
-    active = fields.Boolean(string='Active' default=True)
+    active = fields.Boolean(string='Active', default=True)
